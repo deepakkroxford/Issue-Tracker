5adf1275aba0ab0b9d665715e4d948888cb82e39
deepakkroxford <93421333+deepakkroxford@users.noreply.github.com>
2024-10-03 19:46:35 +0530

    Prisma Setup

--- bug-tracker/app/NavBar.tsx
+++ bug-tracker/app/NavBar.tsx
@@ -1,20 +1,21 @@
+'use client'
 import React from 'react'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import { FaBug } from "react-icons/fa6";
+
 function NavBar() {
 
-    //this is the array with the navigation buttons that will be displayed when the user clicks on the navigation
-
+    const currentPath = usePathname();
+    console.log(currentPath)
     /*
     this code reduce the duplication of the code 
     */
     const links = [
         { name: 'DashBoard', href: '/' },
-        { name: 'Issue', href: '/isuue' },
+        { name: 'Issue', href: '/issues' },
 
     ]
-
-
     return (
         <nav className='flex space-x-7 border-b mb-10 px-5 h-16 items-center'>
             <Link href="/"><FaBug /></Link>
@@ -22,7 +23,7 @@ function NavBar() {
                 {links.map(link => (
                     <li
                         key={link.name}>
-                        <Link className='text-black-300 hover:text-zinc-300 hover:underline' href={link.href}>{link.name}</Link>
+                        <Link className={`text-black-300 hover:text-zinc-300 hover:underline`} href={link.href}>{link.name}</Link>
                     </li>
                 ))}
 
@@ -30,5 +31,4 @@ function NavBar() {
         </nav>
     )
 }
-
 export default NavBar
---
+++ bug-tracker/app/issues/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const IssuesPage = function() {
+  return (
+    <div>IssuePage</div>
+  )
+}
+
+export default IssuesPage
